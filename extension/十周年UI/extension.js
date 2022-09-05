2cd42537c0e00417593e7e5df1b3a46d2477731f
Show-K Adot <88315651+Show-K@users.noreply.github.com>
2022-09-06 00:38:28 +0800

    Mark & Prompt

--- extension/十周年UI/extension.js
+++ extension/十周年UI/extension.js
@@ -714,6 +714,22 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 								game.linexy([x1, y1, x2, y2], config, true);
 							}
 						};
+						Player.prompt = function (str, nature) {
+							var node;
+							if (this.node.prompt) {
+								node = this.node.prompt;
+								node.innerHTML = '';
+								node.className = 'damage normal-font damageadded';
+							} else {
+								node = ui.create.div('.damage.normal-font', this);
+								this.node.prompt = node;
+								ui.refresh(node);
+								node.classList.add('damageadded');
+							}
+							node.innerHTML = str;
+							node.dataset.text = str;
+							node.dataset.nature = nature || 'soil';
+						};
 						Player.checkBoundsCache = function (forceUpdate) {
 							var update;
 							var refer = dui.boundsCaches.arena;
@@ -9998,9 +10014,10 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 				var log = [
 					'有bug先检查其他扩展，不行再关闭UI重试，最后再联系作者。',
 					'当前版本：1.2.0.220114.12SST（Show-K修复版）',
-					'更新日期：2022-09-03',
+					'更新日期：2022-09-05',
 					'- 现在可以设置折叠手牌最小宽度了，且默认值修改为81。',
 					'- 为牌名辅助显示的文字增加了背景颜色，使之更容易阅读。',
+					'- 修复了targetprompt无描边的异常（举例：【借刀杀人】）。',
 					/*
 					'- 新增动皮及背景：[曹节-凤历迎春]、[曹婴-巾帼花舞]、[貂蝉-战场绝版]、[何太后-耀紫迷幻]、[王荣-云裳花容]、[吴苋-金玉满堂]、[周夷-剑舞浏漓]；',
 					'- 新增动皮oncomplete支持(函数内部只能调用this.xxx代码)；',
