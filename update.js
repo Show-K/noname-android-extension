1ecb8d1aafb3a7abeefa382a308d9eddcd29b7d5
Show-K Adot <88315651+Show-K@users.noreply.github.com>
2022-09-15 15:11:13 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 window['noname_android_extension'] = {
 	"在线更新": {
-		"version": "1.43SST",
+		"version": "1.44SST",
 		"author": "诗笺(Show-K修改)",
 		"intro": "用于更新无名杀版本，<span style=\"color: red;\">基本实现0崩溃</span>。",
 		"files": [
